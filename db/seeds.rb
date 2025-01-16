10c6591f77302d638b38a2dcc5fe4f9ec392ba17
Eve Waller <evew1991@outlook.com>
2025-01-16 10:57:26 +0000

    car index and show added

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@ owner_1 = Owner.create(nickname: "Fred")
 owner_2 = Owner.create(nickname: "George")
 owner_3 = Owner.create(nickname: "Ron")
 
-car_1 = Car.create(brand: "Tesla", model: "S", year: 2016, fuel: "Electric", owner: owner_1)
+car_1 = Car.create(brand: "Tesla", model: "Model S", year: 2016, fuel: "Electric", owner: owner_1)
 car_2 = Car.create(brand: "BMW", model: "1 Series", year: 2008, fuel: "Deisel", owner: owner_2)
 car_3 = Car.create(brand: "Audi", model: "A3", year: 2011, fuel: "Diesel", owner: owner_3)
